56cd4e765e84edbecf6f87eaf3d43b6e60cdd8a1
shuvakarmakar <shuva15-7953@diu.edu.bd>
2025-03-16 13:35:45 +0600

    Certificate Update

--- src/Pages/Certification/CertificateData.jsx
+++ src/Pages/Certification/CertificateData.jsx
@@ -5,9 +5,9 @@ import certificate3 from '../../assets/Certificates/Facebook Marketing.jpg';
 import certificate4 from '../../assets/Certificates/Programming Hero Certificate.jpg';
 import certificate5 from '../../assets/Certificates/CodemanBD WordPress.jpg';
 import certificate6 from '../../assets/Certificates/React hackerRank.jpg';
-import certificate7 from '../../assets/Certificates/Coursera N6ZV9YW5SWUU.jpg';
-import certificate8 from '../../assets/Certificates/Coursera 26N7A6GH4MYK.jpg';
-import certificate9 from '../../assets/Certificates/Coursera 5C3LUTWBRMK9.jpg';
+import certificate_Technical from '../../assets/Certificates/Coursera N6ZV9YW5SWUU.jpg';
+import certificate_AI from '../../assets/Certificates/Coursera 26N7A6GH4MYK.jpg';
+import certificate_digital from '../../assets/Certificates/Coursera 5C3LUTWBRMK9.jpg';
 
 const certifications = [
     {
@@ -64,21 +64,21 @@ const certifications = [
         organization: "Coursera",
         issueDate: "June 2020",
         link: "https://coursera.org/share/ec23662779dc819627a3198e8171d89b",
-        image: certificate7,
+        image: certificate_digital,
     },
     {
         title: "Technical Support Fundamentals",
         organization: "Coursera",
         issueDate: "June 2020",
         link: "https://coursera.org/share/0d518f69fa0ac0654af53e75b88f0fb0",
-        image: certificate8,
+        image: certificate_Technical,
     },
     {
         title: "AI For Everyone",
         organization: "Coursera",
         issueDate: "May 2020",
         link: "https://coursera.org/share/6a65a0fc493552ced519a935090e1d65",
-        image: certificate9,
+        image: certificate_AI,
     },
 ];
 
